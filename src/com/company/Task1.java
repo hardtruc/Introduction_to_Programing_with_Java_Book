c253ac852b82b78cf8e3ee2ca8f9d9a45563c393
hardtruc <hardtruc@abv.bg>
2019-11-06 10:44:29 +0200

    First

---
+++ src/com/company/Task1.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Task1 {
+    public static void main (String[] args){
+        int   num1 = 52130;
+        byte  num2 = -115;
+        int   num3 = 4825932;
+        byte  num4 = 97;
+        short num5 = -10000;
+                                            }
+                    }
